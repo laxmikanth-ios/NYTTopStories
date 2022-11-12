64d8decad038f935d0650be38ca308cf6aeb5550
laxmikanthreddy1993 <laxmikanthreddy.serolu@gmail.com>
2022-11-12 10:35:35 +0530

    Initial Commit

---
+++ NYTTopStories/TopStoriesTabController.swift
@@ -0,0 +1,30 @@
+//
+//  TopStoriesTabController.swift
+//  NYTTopStories
+//
+//  Created by Laxmikanth Reddy on 12/11/22.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemYellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
