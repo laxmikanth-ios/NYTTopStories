6b398759856e92265f85fd90972a508be1ddbfd0
laxmikanthreddy1993 <laxmikanthreddy.serolu@gmail.com>
2022-11-13 18:53:02 +0530

    Adding new file Settings View and adding pickerview to that SettingView.

--- NYTTopStories/Controllers/SettingsViewController.swift
+++ NYTTopStories/Controllers/SettingsViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .green
-        // Do any additional setup after loading the view.
+    private let settingsView = SettingsView()
+    
+    // data for pickerview
+    
+    private let sections = ["Arts","Automobiles","Books","Business","Fashion","Food","Health"].sorted() // ascending from a-z
+    
+    override func loadView() {
+        view = settingsView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGroupedBackground
+        
+        // setup pickerview
+        settingsView.pickerView.dataSource = self
+        settingsView.pickerView.delegate = self
     }
-    */
+}
 
+extension SettingsViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sections[row] // accessing each individual string in the sections array
+    }
+}
+
+extension SettingsViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sections.count
+    }
 }
---
+++ NYTTopStories/SettingsView.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsView.swift
+//  NYTTopStories
+//
+//  Created by Laxmikanth Reddy on 13/11/22.
+//
+
+import UIKit
+
+
+class SettingsView: UIView {
+    
+    public lazy var pickerView: UIPickerView = {
+        let pv = UIPickerView()
+        pv.backgroundColor = .white
+        return pv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupPickerViewConstraints()
+    }
+    
+    private func setupPickerViewConstraints() {
+        addSubview(pickerView)
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pickerView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            pickerView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            pickerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            pickerView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+}
+
